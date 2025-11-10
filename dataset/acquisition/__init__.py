db72467b85d5476b0b819d94b30d417248fe4a9b
murilopmachado <murilo.paula.machado@gmail.com>
2025-11-10 17:03:26 +0100

    feat: Complete Firecrawl migration with pipeline cleanup and ASCII reporting

--- dataset/acquisition/__init__.py
+++ dataset/acquisition/__init__.py
@@ -3,11 +3,10 @@ Data Acquisition Module
 
 This module provides access to the different stages of data acquisition:
 - retrieve_url: Retrieves URLs based on questions from qa_questions.json
-- save_screenshot: Captures screenshots from retrieved URLs
+- save_datasource: Scrapes web content using Firecrawl API
 """
 
 from .retrieve_url.main import main as retrieve_url_stage
 from .save_datasource.main import main_async as save_datasource_stage
-from .datasource_processing.main import main as datasource_processing_stage
 
-__all__ = ["retrieve_url_stage", "save_datasource_stage", "datasource_processing_stage"]
+__all__ = ["retrieve_url_stage", "save_datasource_stage"]
