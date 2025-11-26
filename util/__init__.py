458c21e0eb079c360a7298cb518d27611f7c4a10
Umut Ay Bora <umutaybora@gmail.com>
2025-11-26 16:36:52 +0100

    Centralised logging

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
-from .utilities import getEmtpDirectory, getConfig
+from .utilities import getEmtpDirectory, getConfig, getLogger, getConfigValue
+from .ollama.file_upload import upload_file
 
-__all__ = ['getEmtpDirectory', 'getConfig']
+__all__ = ["getEmtpDirectory", "getConfig", "upload_file", "getLogger", "getConfigValue"]
--- util/utilities.py
+++ util/utilities.py
@@ -1,19 +1,36 @@
 import os
 import configparser
+import logging
+from typing import Optional
 
 def getEmtpDirectory():
     current = os.path.dirname(__file__)
-    while current != '':
-        if 'emtp' in os.listdir(current):
+    while current != "":
+        if "emtp" in os.listdir(current):
             break
         current = os.path.dirname(current)
-        if current == '':
-             raise FileNotFoundError("Project root (with 'emtp' folder) not found")
-    path = os.path.join(current, 'emtp')
+        if current == "":
+            raise FileNotFoundError("Project root (with 'emtp' folder) not found")
+    path = os.path.join(current, "emtp")
     return path
-    
-def getConfig(config_name='config.ini'):
+
+
+def getConfig(config_name="config.ini"):
     config = configparser.ConfigParser()
     config_path = os.path.join(getEmtpDirectory(), config_name)
     config.read(config_path)
-    return config    
+    return config
+
+
+def getConfigValue(key, fallback=None, section: str = "DEFAULT"):
+    value = getConfig().get(section, key, fallback=fallback)
+    getConfig().getboolean(section, key, fallback=fallback)
+    return value
+
+def getLogger(name=__name__, verbose: bool = getConfig().getboolean(section="DEFAULT", option="debug_logs", fallback=False)):
+    logging.basicConfig(
+            level=logging.DEBUG if verbose else logging.INFO,
+            format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        )
+    logger = logging.getLogger(name)
+    return logger
