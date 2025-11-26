6fae219b455d9726946471edeb7491a4dd3db433
Umut Ay Bora <umutaybora@gmail.com>
2025-11-26 22:22:25 +0100

    Update __init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,4 @@
-from .utilities import getEmtpDirectory, getConfig, getLogger, getConfigValue
+from .utilities import getEmtpDirectory, getConfig, getLogger
 from .ollama.file_upload import upload_file
 
-__all__ = ["getEmtpDirectory", "getConfig", "upload_file", "getLogger", "getConfigValue"]
+__all__ = ["getEmtpDirectory", "getConfig", "upload_file", "getLogger"]
